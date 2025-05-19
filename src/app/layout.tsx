a4548e30d53782a08f860dad205e348c73c4a6f9
symoh major <simonwanjiru224@gmail.com>
2025-05-19 18:52:33 +0000

    a black kind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body 
         className={`${GeistSans.variable} ${GeistMono.variable} font-sans antialiased flex flex-col min-h-screen`}
       >
